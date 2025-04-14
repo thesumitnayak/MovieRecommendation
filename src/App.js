b9dd38ccfbbde4d6135104dfe85a1d6718b123a4
thesumitnayak <sumit123456nayak@gmail.com>
2025-04-14 21:03:17 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import MovieApp from './components/MovieApp';
+
+const App = () => {
+  return (
+   
+      <div className="App">
+            <MovieApp />
+      </div>
+    
+  );
+};
+
+export default App;
+
